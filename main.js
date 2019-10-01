1f3cf584704f34d1b0534140c7d7cee7c08c49cb
huynhthanhnhan <15520564@gm.uit.edu.vn>
2019-10-01 16:29:53 +0700

    add axis, bouding box,...

--- main.js
+++ main.js
@@ -23,13 +23,21 @@ var createScene = function() {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    var hl = new BABYLON.HighlightLayer("hg", scene);
+    var hl = new BABYLON.HighlightLayer("hl", scene);
+    ////////////////// Bouding Box ///////////////////////
+    var gizmoManager = new BABYLON.GizmoManager(scene);
+    gizmoManager.boundingBoxGizmoEnabled = false;
+    gizmoManager.attachableMeshes = []
+        //  gizmoManager.boundingBoxDragBehavior = null;
+        //  gizmoManager.usePointerToAttachGizmos = false;
+        //  gizmoManager.clearGizmoOnEmptyPointerEvent = true;
 
     // Our built-in 'sphere' shape.
     var line = BABYLON.MeshBuilder.CreateCylinder("line", { height: 10, diameter: 0.05 }, scene);
     line.position.y = 1000;
     var groundTexture = new BABYLON.DynamicTexture("dynamic texture", 512, scene, true);
     var point = BABYLON.MeshBuilder.CreateSphere("point", { diameter: 0.2 }, scene);
+    gizmoManager.attachableMeshes.push(point);
     point.position.y = 1000;
     var pointMat = new BABYLON.StandardMaterial("pointMat", scene);
     pointMat.diffuseColor = new BABYLON.Color3.Red();
@@ -57,6 +65,10 @@ var createScene = function() {
     groundy.material = groundMaterial;
 
     //////////////////// Global Variable ////////////////////
+
+    var listPoint = [];
+    var listLine = [];
+
     var isSelectMode = true;
     var isCreateLineMode = false;
     var isCreateMultiLineMode = false;
@@ -64,6 +76,7 @@ var createScene = function() {
     var isStartCreateLine = true;
 
     var isMultiSelect = false;
+    var isEditMode = false;
 
     var selectedMeshes = [];
 
@@ -127,7 +140,57 @@ var createScene = function() {
         }
     }
 
+    document.getElementById("cbEditMode").onclick = function() {
+        isEditMode = this.checked;
+        gizmoManager.boundingBoxGizmoEnabled = this.checked;
 
+
+        if (this.checked) {
+            var checkboxes = document.getElementsByName('Mode')
+            checkboxes.forEach((item) => {
+                if (item !== this && item.checked) item.click();
+            })
+        }
+    }
+
+
+
+    function CreatePoint(position) {
+        const p = point.clone("point");
+        gizmoManager.attachableMeshes.push(p);
+        // p.isVisible = true;
+        p.position = position;
+        p.actionManager = new BABYLON.ActionManager(scene);
+        p.actionManager.registerAction(
+            new BABYLON.ExecuteCodeAction(
+                BABYLON.ActionManager.OnPointerOverTrigger,
+                function() {
+                    if (isSelectMode && !checkInSelectedMeshes(p)) {
+                        if (hl.hasMesh(p)) hl.removeMesh(p);
+                        hl.addMesh(p, BABYLON.Color3.Yellow());
+                    }
+                }
+            )
+        );
+        p.actionManager.registerAction(
+            new BABYLON.ExecuteCodeAction(
+                BABYLON.ActionManager.OnPointerOutTrigger,
+                function() {
+                    if (isSelectMode && hl.hasMesh(p)) {
+                        if (!checkInSelectedMeshes(p))
+                            hl.removeMesh(p);
+                        // else
+                        //     setTimeout(function() {
+                        //         hl.removeMesh(p);
+                        //         removeFromSelectedMeshes(p);
+                        //     }, 20000);
+
+                    }
+                }
+            )
+        );
+        return p;
+    }
 
     function CreateLine(point1, point2) {
         var distance = BABYLON.Vector3.Distance(point1, point2);
@@ -141,13 +204,18 @@ var createScene = function() {
             newLine.parent = parentLine;
             newLine.rotation.x = Math.PI / 2;
 
+            gizmoManager.attachableMeshes.push(newLine);
+
             newLine.actionManager = new BABYLON.ActionManager(scene);
             newLine.actionManager.registerAction(
                 new BABYLON.ExecuteCodeAction(
                     BABYLON.ActionManager.OnPointerOverTrigger,
                     function() {
-                        if (isCreatePointMode)
+                        if (isCreatePointMode && !checkInSelectedMeshes(p)) {
+                            if (hl.hasMesh(newLine)) hl.removeMesh(newLine);
                             hl.addMesh(newLine, BABYLON.Color3.Yellow());
+                        }
+
                     }
                 )
             );
@@ -155,44 +223,76 @@ var createScene = function() {
                 new BABYLON.ExecuteCodeAction(
                     BABYLON.ActionManager.OnPointerOutTrigger,
                     function() {
-                        if (isCreatePointMode)
-                            hl.removeMesh(newLine);
+                        if (isSelectMode) {
+                            if (!checkInSelectedMeshes(newLine))
+                                hl.removeMesh(newLine);
+                            // else
+                            //     setTimeout(function() {
+                            //         hl.removeMesh(newLine);
+                            //         removeFromSelectedMeshes(newLine);
+                            //     }, 20000);
+                        }
                     }
                 )
             );
         }
     }
 
-    function CreatePoint(position) {
-        const p = point.clone("point");
-        // p.isVisible = true;
-        p.position = position;
-        p.actionManager = new BABYLON.ActionManager(scene);
-        p.actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(
-                BABYLON.ActionManager.OnPointerOverTrigger,
-                function() {
-                    if (isSelectMode)
-                        hl.addMesh(p, BABYLON.Color3.Yellow());
+
+
+    function checkInSelectedMeshes(object) {
+        if (selectedMeshes) {
+            for (var i = 0; i < selectedMeshes.length; i++) {
+                if (selectedMeshes[i] == object) {
+                    return true;
+
                 }
-            )
-        );
-        p.actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(
-                BABYLON.ActionManager.OnPointerOutTrigger,
-                function() {
-                    if (isSelectMode)
-                        hl.removeMesh(p);
+            }
+            return false;
+        }
+
+    }
+
+    function removeFromSelectedMeshes(object) {
+        if (selectedMeshes) {
+            for (var i = 0; i < selectedMeshes.length; i++) {
+                if (selectedMeshes[i] == object) {
+                    selectedMeshes.splice(i, 1);
+                    return;
                 }
-            )
-        );
-        return p;
+            }
+        }
+
+    }
+
+    function removeFromGizmoManagerList(object) {
+        if (gizmoManager.attachableMeshes) {
+            for (var i = 0; i < gizmoManager.attachableMeshes.length; i++) {
+                if (gizmoManager.attachableMeshes[i] == object) {
+                    gizmoManager.attachableMeshes.splice(i, 1);
+                    // refreshGizmoManager();
+                    return;
+                }
+            }
+        }
+
+    }
+
+    function refreshGizmoManager() {
+        gizmoManager.boundingBoxGizmoEnabled = !gizmoManager.boundingBoxGizmoEnabled;
+        gizmoManager.boundingBoxGizmoEnabled = !gizmoManager.boundingBoxGizmoEnabled;
+    }
+
+    function resetSelectedMeshes() {
+        selectedMeshes.forEach(mesh => {
+            if (hl.hasMesh(mesh))
+                hl.removeMesh(mesh);
+            removeFromGizmoManagerList(mesh);
+        })
+        selectedMeshes = [];
     }
 
 
-
-    var p = new BABYLON.Vector3(0, 10, 1);
-    // p.subtract
     scene.onPointerPick = function(evt, pickResult) {
         // if the click hits the ground object, we change the impact position
         if (pickResult.hit) {
@@ -201,6 +301,7 @@ var createScene = function() {
                     const pt = CreatePoint(pickResult.pickedPoint);
                     if (isStartCreateLine) {
                         startPoint = pt;
+                        if (hl.hasMesh(startPoint)) hl.removeMesh(startPoint);
                         hl.addMesh(startPoint, BABYLON.Color3.Green());
                         isStartCreateLine = false;
                     } else {
@@ -212,6 +313,7 @@ var createScene = function() {
                                 isStartCreateLine = true;
                             else {
                                 startPoint = pt;
+                                if (hl.hasMesh(startPoint)) hl.removeMesh(startPoint);
                                 hl.addMesh(startPoint, BABYLON.Color3.Green());
                             }
                         }
@@ -219,6 +321,7 @@ var createScene = function() {
                 } else {
                     if (isStartCreateLine) {
                         startPoint = pickResult.pickedMesh;
+                        if (hl.hasMesh(startPoint)) hl.removeMesh(startPoint);
                         hl.addMesh(startPoint, BABYLON.Color3.Green());
                         isStartCreateLine = false;
                     } else {
@@ -232,64 +335,168 @@ var createScene = function() {
                 }
             } else if (isCreatePointMode) {
                 const pt = CreatePoint(pickResult.pickedPoint);
-            } else {
+            } else if (isSelectMode || isEditMode) {
                 var target = pickResult.pickedMesh;
+                if (hl.hasMesh(target)) hl.removeMesh(target);
                 hl.addMesh(target, BABYLON.Color3.Blue());
                 if (target.name != "groundx" && target.name != "groundy" && target.name != "groundz") {
-                    if (!isMultiSelect) {
+                    if (!isMultiSelect || isEditMode) {
                         selectedMeshes.forEach(mesh => {
                             if (hl.hasMesh(mesh))
                                 hl.removeMesh(mesh);
                         })
-                        selectedMeshes = [];
+                        resetSelectedMeshes();
                     }
                     selectedMeshes.push(target);
+                    gizmoManager.attachableMeshes.push(target);
+                    console.log(gizmoManager.attachableMeshes)
 
+                    // camera.detachControl(canvas);
+                } else {
+                    console.log(target.name)
+                    resetSelectedMeshes();
                 }
-                // setTimeout(function () {
-                //     hl.removeMesh(target);
-                // }, 3000);
             }
 
         }
     };
 
-    /****************************Key Control Multiple Keys************************************************/
+    ////////////////////// Tinh Vector chi phuong cua mat phang ////////////////////
+    var getHorizontalPlaneVector = function(y, pos, rot) {
+        // if (!rot.y) {
+        //     return null;
+        // }
 
-    var map = {}; //object for multiple key presses
-    scene.actionManager = new BABYLON.ActionManager(scene);
+        return new BABYLON.Vector3(
+            //cong thuc tinh vector chi phuong y - y0 = k * ( x - x0 )
+            pos.x - (pos.y - y) * rot.x / rot.y,
+            1,
+            pos.z - (pos.y - y) * rot.z / rot.y
+        );
+    };
 
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function(evt) {
-        map[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+    ///////////////////// Drag mesh with mouse ////////////////////
+    function dragMesh() {
+        var target = BABYLON.Vector3.Unproject(
+            new BABYLON.Vector3(scene.pointerX, scene.pointerY, 0),
+            canvas.width,
+            canvas.height,
+            new BABYLON.Matrix.Identity(),
+            camera.getViewMatrix(),
+            camera.getProjectionMatrix()
+        );
 
-    }));
 
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function(evt) {
-        map[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
-    }));
+        target.x = camera.position.x - target.x;
+        target.y = camera.position.y - target.y;
+        target.z = camera.position.z - target.z;
+
+        var p = getHorizontalPlaneVector(0, camera.position, target);
+        if (selectedMeshes && selectedMeshes.length == 1)
+            selectedMeshes[0].position = p;
+    }
 
 
-    /****************************Key Control******************************************************/
 
-    scene.registerAfterRender(function() {
+    /**************************** Key Control ******************************************************/
 
-        if (map["Control"]) {
-            isMultiSelect = true;
-        } else
-            isMultiSelect = false;
-        if ((map["x"] || map["X"])) {
-            if (selectedMeshes) {
-                selectedMeshes.forEach(mesh => {
-                    if (mesh.parent)
-                        mesh.parent.dispose();
-                    mesh.dispose();
-                })
-            }
+    scene.onKeyboardObservable.add((keyInfo) => {
+        switch (keyInfo.type) {
+            case BABYLON.KeyboardEventTypes.KEYDOWN:
+                switch (keyInfo.event.key) {
+                    case "x" || "X":
+                        if (selectedMeshes) {
+                            selectedMeshes.forEach(mesh => {
+                                if (mesh.parent)
+                                    mesh.parent.dispose();
+                                removeFromGizmoManagerList(mesh);
+                                gizmoManager.attachToMesh(point);
+                                mesh.dispose();
+                            })
+                        }
+                        break;
+                    case "Control":
+                        isMultiSelect = true;
+                        break;
+                    case "s" || "S":
+                        document.getElementById("cbSelectMode").click();
+                        break;
+                    case "p" || "P":
+                        document.getElementById("cbPointMode").click();
+                        break
+                    case "l" || "L":
+                        document.getElementById("cbSingleLineMode").click();
+                        break;
+                    case "m" || "M":
+                        document.getElementById("cbMultiLineMode").click();
+                        break;
+                    case "e" || "E":
+                        document.getElementById("cbEditMode").click();
+                        break;
+                }
+                break;
+            case BABYLON.KeyboardEventTypes.KEYUP:
+                switch (keyInfo.event.key) {
+                    case "Control":
+                        isMultiSelect = false;
+                        break;
+                }
+                break;
+
         }
+    })
 
 
 
+    /**************************** Mouse Control ******************************************************/
+
+    scene.onPointerObservable.add((pointerInfo) => {
+        if (pointerInfo.event.button == 2) {
+            camera.attachControl(canvas);
+        }
+        switch (pointerInfo.type) {
+            // case BABYLON.PointerEventTypes.POINTERDOWN:
+            //     if(pointerInfo.pickInfo.hit) {
+            //         mode = "DRAG";
+            //         castRay();
+            //     }
+            // 	break;
+            case BABYLON.PointerEventTypes.POINTERMOVE:
+                // if(pickedMesh && mode === "DRAG"){
+                // dragMesh();
+                // }                
+                break;
+            case BABYLON.PointerEventTypes.POINTERUP:
+                // if (pickedMesh) 
+                {
+                    // mode = "CAMERA";
+                    // pickedMesh = null;
+                    // camera.attachControl(canvas);
+                }
+                break;
+            case BABYLON.PointerEventTypes.POINTERTAP:
+                // if (pointerInfo.pickInfo.hit) {
+                //     // mode = "GIZMO";
+                //     // castRay();
+                //     if (selectedMeshes && selectedMeshes.length == 1) {
+                //         gizmoManager.attachToMesh(selectedMeshes[0]);
+                //     }
+                // }
+                break;
+
+        }
     });
+
+    scene.registerBeforeRender(function() {
+        // if (selectedMeshes.length >= 1) {
+        //     selectedMeshes.forEach(mesh => {
+        //         if (mesh)
+        //             gizmoManager.attachToMesh(mesh);
+        //     })
+        // }
+        // console.log(isMultiSelect);
+    })
+
 
     // var mousePoint = point.clone();
     // scene.onPointerMove = function(evt, pickResult){
@@ -299,6 +506,45 @@ var createScene = function() {
     //         mousePoint.position = pickResult.pickedPoint;
     //     }
     // }
+
+    ///////////////// Show Axis ////////////////////////
+    var showAxis = function(size) {
+        var makeTextPlane = function(text, color, size) {
+            var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+            dynamicTexture.hasAlpha = true;
+            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+            var plane = new BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+            plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+            plane.material.backFaceCulling = false;
+            plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+            plane.material.diffuseTexture = dynamicTexture;
+            return plane;
+        };
+
+        var axisX = BABYLON.Mesh.CreateLines("axisX", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+        ], scene);
+        axisX.color = new BABYLON.Color3(1, 0, 0);
+        var xChar = makeTextPlane("X", "red", size / 10);
+        xChar.position = new BABYLON.Vector3(size, -0.1 * size, 0);
+        var axisY = BABYLON.Mesh.CreateLines("axisY", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+        ], scene);
+        axisY.color = new BABYLON.Color3(0, 1, 0);
+        var yChar = makeTextPlane("Y", "green", size / 10);
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, 0);
+        var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+        ], scene);
+        axisZ.color = new BABYLON.Color3(0, 0, 1);
+        var zChar = makeTextPlane("Z", "blue", size / 10);
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+    };
+
+    showAxis(10);
 
     return scene;
 
@@ -320,7 +566,6 @@ engine.runRenderLoop(function() {
 window.addEventListener("resize", function() {
     engine.resize();
 });
-
 
 
 // https://www.babylonjs-playground.com/#T3DJ2B#3
